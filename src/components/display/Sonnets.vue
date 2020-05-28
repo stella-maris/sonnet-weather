befc43d6afd88a889dd4114a2da67eaf1d35f375
stella-maris <mango.gal@gmail.com>
2020-05-28 15:45:39 -0600

    adds missing router

--- src/components/display/Sonnets.vue
+++ src/components/display/Sonnets.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <div>WORLD</div>
 </template>
 
 <script>
--- src/widgets/WeatherWidget.vue
+++ src/widgets/WeatherWidget.vue
@@ -1,14 +1,16 @@
 <template>
     <div id="weatherWidget">
         Hello
+        <Sonnets/>
     </div>
 </template>
 
 <script>
+import Sonnets from '@/components/display/Sonnets.vue';
     export default {
         name: "WeatherWidget",
         components: {
-
+            Sonnets,
         }
     }
 </script>
